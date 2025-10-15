d4405e11a72d7dae7d22cc1b978d249475ec49a4
carmencatrodeza-blip <carmencatrodeza@gmail.com>
2025-10-15 20:40:48 +0200

    toString() mejorado

--- TablaSimbolos.java
+++ TablaSimbolos.java
@@ -1,16 +1,18 @@
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Arrays;
 
 public class TablaSimbolos {
 
     // Estructura interna de cada símbolo
     public static class Simbolo {
-        private final Integer pos;     // posición en la TS (empieza en 1)
+        // private final int id; // Hará falta para cuando hagamos una TS por función.
+        private final Integer pos; // posición en la TS (empieza en 1)
         private final String tipo; // "id", "int", "float", etc. (si lo necesitas más tarde)
         private final Integer desplazamiento; 
-        private final Integer numParams;
         private final String[] tiposParams;
         private final int[] modosPasoParams; // 1: por valor, 2: por referencia
+        private final Integer numParams;
         private final String tipoRetorno;
         private final String etiqueta;
 
@@ -18,9 +20,9 @@ public class TablaSimbolos {
             this.pos = pos;
             this.tipo = null;
             this.desplazamiento = null;
-            this.numParams = null;
             this.tiposParams = null;
             this.modosPasoParams = null;
+            this.numParams = null;
             this.tipoRetorno = null;
             this.etiqueta = null;
         }
@@ -29,21 +31,21 @@ public class TablaSimbolos {
             this.pos = pos;
             this.tipo = tipo;
             this.desplazamiento = desplazamiento;
-            this.numParams = null;
             this.tiposParams = null;
             this.modosPasoParams = null;
+            this.numParams = null;
             this.tipoRetorno = null;
             this.etiqueta = null;
         }
 
-        public Simbolo(int pos, String tipo, int numParams, String[] tiposParams, int[] modosPasoParams,
+        public Simbolo(int pos, String tipo, String[] tiposParams, int[] modosPasoParams,
                         String tipoRetorno, String etiqueta){ // Constructor para identificadores de funciones
             this.pos = pos;
             this.tipo = tipo;
             this.desplazamiento = null;
-            this.numParams = numParams;
-            this.tiposParams = tiposParams;
-            this.modosPasoParams = modosPasoParams;
+            this.tiposParams = Arrays.copyOf(tiposParams, tiposParams.length);
+            this.modosPasoParams = Arrays.copyOf(modosPasoParams, modosPasoParams.length);
+            this.numParams = tiposParams.length;
             this.tipoRetorno = tipoRetorno;
             this.etiqueta = etiqueta;
         }
@@ -59,8 +61,8 @@ public class TablaSimbolos {
     }
 
     // Usamos LinkedHashMap para conservar orden de inserción (posiciones deterministas)
-    private final LinkedHashMap<String, Simbolo> tabla = new LinkedHashMap<>();
-    private int siguientePos = 1;
+    private final LinkedHashMap<String, Simbolo> tabla = new LinkedHashMap<>(); // identificador -> simbolo
+    private int siguientePos = 1; // ? Realmente es util?
     private int sumDesplazamiento = 0; 
 
     /** Devuelve la posición del identificador si existe; -1 si no está. */
@@ -113,10 +115,28 @@ public class TablaSimbolos {
 
         for(Map.Entry<String,Simbolo> entry : tabla.entrySet()){
             String lexema = entry.getKey();
-            sb.append("* LEXEMA: '").append(lexema).append("'\n");
-            sb.append("      Atributos:\n");
+            imprimirSimbolo(lexema, entry.getValue(), sb);
+            sb.append("--------- ---------\n");
         }
-        sb.append("___________________________\n");
+        sb.append("---------------------------------------------------\n");
         return sb.toString();
+    }
+
+    private void imprimirSimbolo(String id, Simbolo s, StringBuilder sb) {
+        sb.append("* LEXEMA: '").append(id).append("'\n");
+        sb.append("  Atributos:\n");
+        if (s.getTipo() != null)
+            sb.append("  + tipo: ").append(s.getTipo()).append("\n");
+        if (s.getDesplazamiento() != null)
+            sb.append("  + despl: ").append(s.getDesplazamiento()).append("\n");
+        if (s.getNumParams() != null) {
+            sb.append("  + numParam: ").append(s.getNumParams()).append("\n");
+            for (int i = 0; i < s.getNumParams(); i++) {
+                sb.append("    + tipoParam").append(i + 1).append(": ").append(s.getTiposParams()[i]).append("\n");
+                sb.append("    modoParam").append(i + 1).append(": ").append(s.getModosPasoParams()[i]).append("\n");
+            }
+            sb.append("    + tipoRetorno: ").append(s.getTipoRetorno()).append("\n");
+            sb.append("  + etiqueta: ").append(s.getEtiqueta()).append("\n");
+        }
     }
 }
